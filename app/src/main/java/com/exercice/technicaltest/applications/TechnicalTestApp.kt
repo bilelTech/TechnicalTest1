fb82b28300a74cf96512cde967d235c69cfa428a
Bilel Najjar <bilel.najjar2013@gmail.com>
2024-02-28 13:40:59 +0100

    add application class

--- app/src/main/java/com/exercice/technicaltest/applications/TechnicalTestApp.kt
+++ app/src/main/java/com/exercice/technicaltest/applications/TechnicalTestApp.kt
@@ -1,4 +1,8 @@
 package com.exercice.technicaltest.applications
 
-class TechnicalTestApp {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TechnicalTestApp : Application(){
 }
